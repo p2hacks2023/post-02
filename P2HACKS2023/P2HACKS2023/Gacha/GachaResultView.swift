6a0eee906d8513349e52e131db1fcae4c5fa6725
Takumi130902 <kodamat4sea1392@gmail.com>
2023-12-16 21:08:17 +0900

    フロントエンドの各画面UIの追加

---
+++ P2HACKS2023/P2HACKS2023/Gacha/GachaResultView.swift
@@ -0,0 +1,18 @@
+//
+//  GachaResultView.swift
+//  P2HACKS2023
+//
+//  Created by 児玉拓海 on 2023/12/16.
+//
+
+import SwiftUI
+
+struct GachaResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GachaResultView()
+}
---
+++ P2HACKS2023/P2HACKS2023/View.swift
@@ -0,0 +1,10 @@
+//
+//  SwiftUIView.swift
+//  P2HACKS2023
+//
+//  Created by 児玉拓海 on 2023/12/16.
+//
+
+import SwiftUI
+
+
