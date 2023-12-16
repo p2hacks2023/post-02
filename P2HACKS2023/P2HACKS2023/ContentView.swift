83b11cf23baf2f0660ae9ed68ae79eff0c5c7414
Ryunosuke-Kobayashi <b1020197@fun.sc.jp>
2023-12-16 13:16:24 +0900

    音声認識機能追加

--- P2HACKS2023/P2HACKS2023/ContentView.swift
+++ P2HACKS2023/P2HACKS2023/ContentView.swift
@@ -5,20 +5,48 @@
 //  Created by 児玉拓海 on 2023/12/12.
 //
 
+
 import SwiftUI
+
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        AudioRecognizedView()
     }
 }
 
-#Preview {
-    ContentView()
+//音声認識するために便宜上作ったView
+struct AudioRecognizedView: View {
+    @ObservedObject var speechManager = SpeechManager()
+    
+    var body: some View{
+        VStack {
+            
+            //音声認識が始まる
+            Text("Recognized Text: \(speechManager.recognizedText)")
+                .padding()
+            
+            Button("Start Recording") {
+                speechManager.startRecording()
+            }
+            .padding()
+            
+            //音声認識されたテキストデータを更新したかった。以下のコードではできない
+            Button("Reset Recording") {
+                speechManager.resetRecognizedText()
+            }
+            .padding()
+            
+            Button("Stop Recording") {
+                speechManager.stopRecording()
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
